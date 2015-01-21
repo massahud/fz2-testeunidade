5c54fa0ddae3cae88029bf863c598b8ee795f6d9
massahud <massahud@gmail.com>
2015-01-21 09:11:23 -0200

    doctrine conf

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+  'doctrine' => array(
+    'connection' => array(
+      'orm_default' => array(
+        'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
+        'params' => array(
+          'host'     => 'localhost',
+          'port'     => '3306',
+          'user'     => 'forum',
+          'password' => 'senha_forum',
+          'dbname'   => 'forum',
+)))));
